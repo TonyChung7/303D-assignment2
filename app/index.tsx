e42327ebae501c5ed2901ae80dd29c1864c25cb8
TonyChung7 <shihwei.chung@edu.sait.ca>
2026-02-07 21:14:24 -0700

    add main section to index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,8 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import {
+  Dimensions,
+  Image,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -14,6 +16,8 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+
+const { width: windowWidth } = Dimensions.get("window");
 
 export default function Index() {
   return (
@@ -34,7 +38,86 @@ export default function Index() {
       <ScrollView
         contentContainerStyle={styles.scrollContent}
         showsVerticalScrollIndicator={false}
-      ></ScrollView>
+      >
+        <View style={styles.userRow}>
+          <View style={styles.userInfo}>
+            <Image
+              source={require("../assets/images/avatar.jpg")}
+              style={styles.avatar}
+            />
+            <View>
+              <Text style={styles.username}>ootd_everyday</Text>
+              <Text style={styles.subText}>via frenchie_fry39</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.settingBtn}>
+            <Text style={styles.settingText}>⋯</Text>
+          </TouchableOpacity>
+        </View>
+        <Image
+          source={require("../assets/images/post.jpg")}
+          style={styles.postImage}
+          resizeMode="cover"
+        />
+        <View style={styles.iconRow}>
+          <View style={styles.leftIconsGroup}>
+            <TouchableOpacity style={styles.iconBtn}>
+              <Ionicons name="heart-outline" size={26} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconBtn}>
+              <Ionicons name="chatbubble-outline" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconBtn}>
+              <Ionicons name="paper-plane-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.iconBtn}>
+              <Ionicons name="bookmark-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.contentSection}>
+          <View style={styles.likesRow}>
+            <Image
+              source={require("../assets/images/avatar.jpg")}
+              style={styles.avatarlikes}
+            />
+            <Image
+              source={require("../assets/images/avatar.jpg")}
+              style={styles.avatarlikes}
+            />
+            <Image
+              source={require("../assets/images/avatar.jpg")}
+              style={styles.avatarlikes}
+            />
+            <Text style={styles.likestext}>
+              Liked by
+              <Text style={{ fontWeight: "bold" }}> paisley.print.48 </Text>
+              and
+              <Text style={{ fontWeight: "bold" }}> 7 others </Text>
+            </Text>
+          </View>
+          <View style={styles.userPost}>
+            <Text style={styles.contentText}>
+              <Text style={styles.username}>frenchie_fry39 </Text>
+              Fresh shot on a sunny day! ☀️
+            </Text>
+          </View>
+          <Text style={styles.viewComment}>View all 12 comments</Text>
+          <View>
+            <Text style={styles.contentText}>
+              <Text style={styles.username}>lil_wyatt838 </Text>
+              Awesome tones
+            </Text>
+            <Text style={styles.contentText}>
+              <Text style={styles.username}>pia.in.a.pod </Text>
+              Gorg. Love it!❤️
+            </Text>
+          </View>
+          <Text style={styles.postTime}>1 day ago</Text>
+        </View>
+      </ScrollView>
 
       {/* Bottom Nav */}
       <View style={styles.bottomNav}>
@@ -104,7 +187,118 @@ const styles = StyleSheet.create({
   },
 
   scrollContent: {
-    paddingBottom: 0,
+    paddingBottom: 38,
+    width: windowWidth,
+    maxWidth: windowWidth,
+  },
+
+  userRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+
+  userInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 10,
+  },
+
+  username: {
+    fontWeight: "600",
+    fontSize: 15,
+  },
+
+  subText: {
+    fontSize: 12,
+    color: "#8e8e8e",
+  },
+
+  settingBtn: {
+    width: 36,
+    height: 36,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  settingText: {
+    fontSize: 15,
+  },
+
+  postImage: {
+    width: windowWidth,
+    height: undefined,
+    aspectRatio: 4 / 5,
+  },
+
+  iconRow: {
+    flexDirection: "row",
+    padding: 6,
+    justifyContent: "space-between",
+  },
+
+  leftIconsGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+
+  iconBtn: {
+    width: 40,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  contentSection: {
+    paddingHorizontal: 10,
+    gap: 6,
+  },
+
+  likesRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  avatarlikes: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginLeft: -3,
+  },
+
+  likestext: {
+    fontSize: 13,
+    color: "#111",
+    marginLeft: 6,
+  },
+
+  userPost: {
+    flexDirection: "row",
+  },
+
+  contentText: {
+    flex: 1,
+    fontSize: 15,
+    color: "#111",
+  },
+
+  viewComment: {
+    fontSize: 13,
+    color: "#8e8e8e",
+    marginTop: 4,
+  },
+
+  postTime: {
+    fontSize: 11,
+    color: "#8e8e8e",
   },
 
   bottomNav: {
